6f9f6d7021b11fde579eee09cfea244ccf606970
Julius Mboto <juliusmboto@gmail.com>
2024-06-27 22:02:09 +0300

    feat: add model, migration, factory, create task api

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
+
