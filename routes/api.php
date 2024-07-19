fd0ff060f1f557ea421daece13d162e850a5bde4
Julius Mboto <juliusmboto@gmail.com>
2024-07-19 14:53:01 +0300

    feat: disable API V1

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 Route::prefix('auth')->group(function () {
     Route::post('login', LoginController::class);
@@ -17,7 +17,7 @@ Route::prefix('auth')->group(function () {
 });
 
 
-require __DIR__ . '/api/v1.php';
+// require __DIR__ . '/api/v1.php';
 require __DIR__ . '/api/v2.php';
 
 
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,9 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::prefix('auth')->group(function () {
-    Route::post('login', LoginController::class)->middleware('guest');
-    Route::post('logout', LogoutController::class);
-    Route::post('register', RegisterController::class);
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('auth');
+    Route::post('/register', RegisterController::class)->middleware('guest');
 });
